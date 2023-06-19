51081c62e9ccf68a73dc416d9452f35f06f806a4
sunrise <1084698040@qq.com>
2023-06-20 01:17:29 +0800

    finish the unet_standalong and need to debug it

--- examples/BPU/infer_unet_standalong.cpp
+++ examples/BPU/infer_unet_standalong.cpp
@@ -85,7 +85,7 @@ void postprocess(const cv::Mat outputs, cv::Mat &pred)
 
 
 void infer_unet();
-void check_preprocess();
+void check_all();
 
 int main(int argc, char **argv)
 {
@@ -98,6 +98,8 @@ int main(int argc, char **argv)
 
   if (mode == "infer")
     infer_unet();
+  else if (mode == "check")
+    check_all();
   else
     CV_Error(cv::Error::StsAssert, "Unknown mode: " + mode);
 
@@ -239,8 +241,173 @@ void infer_unet()
   hbDNNRelease(packed_dnn_handle);
 }
 
-
-void check_preprocess()
+std::vector<size_t> get_shape(cv::Mat &mat)
 {
+  CV_Assert(mat.rows < 0);
+
+  std::vector<size_t> shapes;
+  for(int k = 0; k < mat.size.dims(); k++)
+    shapes.push_back(mat.size[k]);
   
+  return shapes;
 }
+
+std::vector<int> get_dims(const cnpy::NpyArray &arr)
+{
+  std::vector<int> dims;
+  for (auto dim : arr.shape)
+    dims.push_back(dim);
+  return dims;
+}
+
+
+void check_preprocess(const cnpy::NpyArray &arr_image, cv::Mat &datain)
+{
+  // Load image
+  cv::Mat img;
+  CV_Assert(arr_image.shape.size() == 3);
+  img.create(arr_image.shape[0], arr_image.shape[1], CV_MAKETYPE(CV_8U, arr_image.shape[2]));
+  memcpy(img.data, arr_image.data<unsigned char>(), img.total() * img.elemSize());
+
+  // Get datain
+  preprocess_onboard(img, 256, 256, datain);
+}
+
+void check_postprocess(const cnpy::NpyArray &arr_dataout, cv::Mat &pred)
+{
+  // Load image
+  cv::Mat dataout;
+  std::vector<int> dims = get_dims(arr_dataout);
+  dataout.create(dims.size(), dims.data(), CV_32FC1);
+  memcpy(dataout.data, arr_dataout.data<unsigned char>(), dataout.total() * dataout.elemSize());
+
+  // Get pred
+  postprocess(dataout, pred);
+}
+
+void check_infer(const std::string &binpath, const cnpy::NpyArray &arr_datain, cv::Mat &dataout)
+{
+  // Load image
+  cv::Mat datain;
+  CV_Assert(arr_datain.shape.size() == 4);
+  std::vector<int> dims = get_dims(arr_datain);
+  datain.create(dims.size(), dims.data(), CV_8UC1);
+  memcpy(datain.data, arr_datain.data<unsigned char>(), datain.total() * datain.elemSize());
+
+  // -----------------模型加载部分--------------------
+  // 1. 加载BIN模型集
+  hbPackedDNNHandle_t packed_dnn_handle; // 模型集合指针
+  const char *model_file_name = binpath.c_str();
+  hbDNNInitializeFromFiles(&packed_dnn_handle, &model_file_name, 1);
+
+  // 2. 提取模型集中所有的模型名称
+  const char **model_name_list;
+  int model_count = 0;
+  hbDNNGetModelNameList(&model_name_list, &model_count, packed_dnn_handle);
+  for (int k = 0; k < model_count; k++) // 输出提取出的所有模型的名称
+    LOG(INFO) << "Parsed Model Name: " << std::string(model_name_list[k]);
+  // 3. 利用目标模型名提取模型指针
+  hbDNNHandle_t dnn_handle; // ※模型指针
+  hbDNNGetModelHandle(&dnn_handle, packed_dnn_handle, model_name_list[0]);
+
+  // -----------------输入输出内存分配--------------------
+  // 1. 获取输入/输出Tensor个数
+  int input_tensornum = 0, output_tensornum = 0;
+  hbDNNGetInputCount(&input_tensornum, dnn_handle);
+  hbDNNGetOutputCount(&output_tensornum, dnn_handle);
+  LOG(INFO) << "input tensor num: " << input_tensornum << ", output tensor num: " << output_tensornum;
+
+  // 2. 获取输入/输出Tensor参数
+  std::vector<hbDNNTensorProperties> input_properties, output_properties; // 输入/输出Tensor参数
+  input_properties.resize(input_tensornum), output_properties.resize(output_tensornum);
+  for (int k = 0; k < input_tensornum; k++)
+    hbDNNGetInputTensorProperties(&input_properties[k], dnn_handle, k);
+  for (int k = 0; k < output_tensornum; k++)
+    hbDNNGetOutputTensorProperties(&output_properties[k], dnn_handle, k);
+
+  // 3. 利用参数分配Tensor内存
+  std::vector<hbDNNTensor> input_tensors, output_tensors; // ※输入/输出Tensor
+  input_tensors.resize(input_tensornum), output_tensors.resize(output_tensornum);
+  for (int k = 0; k < input_tensornum; k++)
+  {
+    const auto &property = input_properties[k];
+    input_tensors[k].properties = property;
+    hbSysAllocCachedMem(&input_tensors[k].sysMem[0], property.alignedByteSize);
+  }
+  for (int k = 0; k < output_tensornum; k++)
+  {
+    const auto &property = output_properties[k];
+    output_tensors[k].properties = property;
+    hbSysAllocCachedMem(&output_tensors[k].sysMem[0], property.alignedByteSize);
+  }
+  LOG(INFO) << "Finish initializing input/output tensors";
+
+
+  // -----------------模型推理：预处理→BPU推理→后处理--------------------
+  auto &tensor = input_tensors[0];
+  tensor.properties.alignedShape = tensor.properties.validShape;
+
+  // 2. 预处理数据memcpy至BPU
+  memcpy(tensor.sysMem[0].virAddr, datain.data, datain.total() * datain.elemSize());
+  
+  // 3. 刷新CPU数据到BPU
+  hbSysFlushMem(&tensor.sysMem[0], HB_SYS_MEM_CACHE_CLEAN);
+  // 4. 推理模型
+
+  hbDNNTaskHandle_t task_handle = nullptr; // 任务句柄
+  hbDNNInferCtrlParam infer_ctrl_param;
+  HB_DNN_INITIALIZE_INFER_CTRL_PARAM(&infer_ctrl_param);
+  auto ptr_outtensor = output_tensors.data();
+  hbDNNInfer(&task_handle, &ptr_outtensor, input_tensors.data(), dnn_handle, &infer_ctrl_param);
+
+  // 5. 等待任务结束
+  hbDNNWaitTaskDone(task_handle, 0);
+  // 6. 释放任务
+  hbDNNReleaseTask(task_handle);
+  // 7. 刷新BPU数据到CPU
+  hbSysFlushMem(&(output_tensors[0].sysMem[0]), HB_SYS_MEM_CACHE_INVALIDATE);
+  // 8. 从Tensor地址memcpy后处理数据
+  dataout.create(output_tensors[0].properties.alignedShape.numDimensions, 
+                 output_tensors[0].properties.alignedShape.dimensionSize, CV_32FC1);
+  memcpy(dataout.data, (unsigned char *)output_tensors[0].sysMem[0].virAddr, dataout.total() * dataout.elemSize());
+  LOG(INFO) << "Finish infer";
+
+  //////////// 模型推理：预处理→BPU推理→后处理 ////////////
+  // 1. 释放内存
+  for (auto &input : input_tensors)
+    hbSysFreeMem(&(input.sysMem[0]));
+  for (auto &output : output_tensors)
+    hbSysFreeMem(&(output.sysMem[0]));
+
+  // 2. 释放模型
+  hbDNNRelease(packed_dnn_handle);
+}
+
+
+
+void check_all()
+{
+  std::string dataroot = "projects/torchdnn/data/unet/";
+  std::string npzpath = dataroot + "unet_checkstage2.npz";
+  std::string binpath = dataroot + "model_output/unet.bin";
+  std::string savepath = dataroot + "unet_checkcppresults.npz";
+
+  cnpy::npz_t datanpz = cnpy::npz_load(npzpath);
+
+  cv::Mat datain, dataout, pred;
+
+  LOG(INFO) << "Start check_preprocess";
+  check_preprocess(datanpz["image"], datain);
+  LOG(INFO) << "Start check_postprocess";
+  check_postprocess(datanpz["dataout"], pred);
+  LOG(INFO) << "Start check_infer";
+  check_infer(binpath, datanpz["datain"], dataout);
+
+  LOG(INFO) << "Start saving results";
+  cnpy::npz_save(savepath, "datain", (unsigned char*)datain.data, get_shape(datain), "w"); 
+  cnpy::npz_save(savepath, "dataout", (float*)dataout.data, get_shape(dataout), "a"); 
+  cnpy::npz_save(savepath, "pred", (unsigned char*)pred.data, get_shape(pred), "a"); 
+  LOG(INFO) << "Finish saving results in " << savepath;
+
+
+} 

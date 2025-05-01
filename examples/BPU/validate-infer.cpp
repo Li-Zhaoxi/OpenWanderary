f69538b59c0cf22f2d6d25caab6ea5bd3fd38b94
Zhaoxi.Li <1084698040@qq.com>
2025-05-01 16:22:05 +0800

    Add support for rdk x5

--- examples/BPU/validate-infer.cpp
+++ examples/BPU/validate-infer.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 #include <glog/logging.h>
 #include <gflags/gflags.h>
 
---
+++ wanderary/common/color_conversion.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace wanderary {
+
+
+  
+} // namespace wanderary

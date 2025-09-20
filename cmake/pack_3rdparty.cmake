a1bafaee329d367bbfa8cdced2731b68964f033b
Li-Zhaoxi <1084698040@qq.com>
2025-09-20 09:53:24 +0800

    Fix cmake bugs to make it usable in other projects

--- cmake/pack_3rdparty.cmake
+++ cmake/pack_3rdparty.cmake
@@ -5,3 +5,8 @@ set_target_properties(hobot::multimedia PROPERTIES
 IMPORTED_LOCATION "/usr/hobot/lib/libmultimedia.so"
 INTERFACE_INCLUDE_DIRECTORIES "/usr/include"
 )
+
+add_library(bpu::cnn_intf SHARED IMPORTED GLOBAL)
+set_target_properties(bpu::cnn_intf PROPERTIES
+  IMPORTED_LOCATION "/usr/hobot/lib/libcnn_intf.so"
+)
